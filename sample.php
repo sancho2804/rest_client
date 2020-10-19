0cdf040173ac6d321a3fddc306950ef281d5d82f
sancho2804 <sancho2804@mail.ru>
2020-10-20 00:32:05 +0300

    Поправил автозагрузку

--- sample.php
+++ sample.php
@@ -1,11 +1,12 @@
 <?php
-include 'class.php'; //Подключаем файл с классом
+// include 'class.php'; //Подключаем файл с классом
+include 'vendor/autoload.php';
 include 'token.php'; //Для примера. В этом файле хранится только токен
 
-use sancho2804\rest_client; //Используем namespace 
+use sancho2804\rest_client\main; //Используем namespace 
 
 //Создаем объект класса rest_client со ссылкой REST API. Если требуется атворизация по OAuth, то в логин передаем OAuth, а в пароль токен
-$rest=new rest_client\init('https://cloud-api.yandex.net:443/v1/','OAuth',$token);
+$rest=new main('https://cloud-api.yandex.net:443/v1/','OAuth',$token);
 
 $result=$rest->get('disk/resources?path=/');//Получаем информацию о директории
 var_dump($result);//Выводим результат
